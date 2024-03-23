9f5a322d7d409f83237d77b9180945bf0d202d2c
Wendel Scardua <wendelscardua@gmail.com>
2024-03-23 00:05:21 -0300

    Fix save ram

--- src/config.s
+++ src/config.s
@@ -1,13 +1,15 @@
                                 ; config.s
         .global __chr_rom_size
-        .global __prg_ram_size
+        .global __prg_nvram_size
         .global __four_screen
         .global __mirroring
+        .global __battery
 
                                 ; Kilobytes
         __prg_rom_size = 64
         __chr_rom_size = 8
-        __prg_ram_size = 8
+        __prg_nvram_size = 8
+        __battery = 1
 
                                 ; Flags
         __four_screen = 0
